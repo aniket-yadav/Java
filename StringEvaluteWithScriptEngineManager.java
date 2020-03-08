90a5a610d61b1ee6c731697c3573c060ea87a2c9
Aniket Yadav <aniket4897@gmail.com>
2020-03-08 16:27:53 +0530

    Update and rename script.java to StringEvaluteWithScriptEngineManager.java

--- StringEvaluteWithScriptEngineManager.java
+++ StringEvaluteWithScriptEngineManager.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import javax.script.*;
-class script{
+class StringEvaluteWithScriptEngineManager{
 	public static void main(String[] args) throws Exception{
+		//ScriptEngineManager let you use Javascript
 		ScriptEngineManager mgr= new ScriptEngineManager();
 		ScriptEngine engine= mgr.getEngineByName("JavaScript");
-
+		//using javascript method eval to evaluate math expresition
 		System.out.println(engine.eval("34+785-55*23/5"));
 	}
 }
